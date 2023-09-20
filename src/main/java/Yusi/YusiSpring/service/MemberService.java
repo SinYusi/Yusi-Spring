f780c81a0f7e12c4c62ed1eae148d758097676a6
Yusi <yusi4781@naver.com>
2023-09-20 21:53:22 +0900

    CH_26

--- src/main/java/Yusi/YusiSpring/service/MemberService.java
+++ src/main/java/Yusi/YusiSpring/service/MemberService.java
@@ -25,10 +25,20 @@ public class MemberService {
      *회원 가입
      */
     public Long join(Member member){
-        //같은 이름이 있는 중복 회원 X
-        validateDuplicateMember(member);//중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+        //밀리세컨트로 시작하는 시간
+
+        try {
+            //같은 이름이 있는 중복 회원 X
+            validateDuplicateMember(member);//중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally{
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = "+timeMs + "ms");
+        }
     }
     //중복 회원 검증
     private void validateDuplicateMember(Member member) {
@@ -46,7 +56,14 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try{
+            return memberRepository.findAll();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers : " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId){
